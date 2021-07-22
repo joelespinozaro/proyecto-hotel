8a86e738ad16438cdcb844e0de2118d59b71e579
Joel Espinoza Rojas <joelespinozaro@gmail.com>
2021-07-21 22:53:58 -0500

    [add] login and register form

---
+++ features/api/user.ts
@@ -0,0 +1,56 @@
+import axios from "axios";
+
+const SERVER_BASE_URL = "/api/";
+
+const UserAPI = {
+  current: async () => {
+    const user: any = window.localStorage.getItem("user");
+    const token = user?.token;
+    try {
+      const response = await axios.get(`/user`, {
+        headers: {
+          Authorization: `Token ${encodeURIComponent(token)}`,
+        },
+      });
+      return response;
+    } catch (error) {
+      return error.response;
+    }
+  },
+  login: async (email, password) => {
+    try {
+      const response = await axios.post(
+        `${SERVER_BASE_URL}/users/login`,
+        JSON.stringify({ user: { email, password } }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      return response;
+    } catch (error) {
+      return error.response;
+    }
+  },
+  register: async (username, email, password) => {
+    try {
+      const response = await axios.post(
+        `${SERVER_BASE_URL}/users`,
+        JSON.stringify({ user: { username, email, password } }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      return response;
+    } catch (error) {
+      return error.response;
+    }
+  },
+
+  get: async (username) => axios.get(`${SERVER_BASE_URL}/profiles/${username}`),
+};
+
+export default UserAPI;
---
+++ pages/user/register.tsx
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+
+// import CustomLink from "../../components/common/CustomLink";
+import RegisterForm from "../../features/common/RegisterForm";
+
+const Register = () => (
+  <>
+    <Head>
+      <title>REGISTER | NEXT REALWORLD</title>
+      <meta name="description" content="Please register before login" />
+    </Head>
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign Up</h1>
+            <p className="text-xs-center">
+              <Link href="/user/login" as="/user/login">
+                Have an account?
+              </Link>
+            </p>
+
+            <RegisterForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
+);
+
+export default Register;
